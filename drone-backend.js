a3d5a80ddaab7571826d585c12669a9f8fa17999
V5805 (Course ID) <v5805@cam.ac.uk>
2015-08-14 14:36:46 +0100

    drone-test

--- drone-backend.js
+++ drone-backend.js
@@ -11,11 +11,42 @@ Cylon.robot()
         driver: "ardrone",
         connection: "ardrone"
     })
+    .device("nav", {
+        driver: "ardrone-nav",
+        connection: "ardrone"
+})
     .on("ready", fly);
     
 // Fly the bot
 function fly(robot) {
+    bot = robot;
+    bot.drone.config('general:navdata_demo', 'TRUE');
+    bot.nav.on("navdata", function(data) {
+    });
 
+    bot.nav.on("altitudeChange", function(data) {
+        console.log("Altitude:", data);
+        // Drone is higher than 1.5 meters up
+        if (data > 1.5) {
+            bot.drone.land();
+        }
+    });
+
+    bot.drone.disableEmergency();
+    bot.drone.ftrim();
+    bot.drone.takeoff();
+    after(5*1000, function() {
+        bot.drone.left(0.2);
+    });
+    after(7*1000, function() {
+        bot.drone.left(0);
+    });
+    after(10*1000, function() {
+        bot.drone.land();
+    });
+    after (15*1000, function(){
+        bot.drone.stop();
+    });
 }
 
 Cylon.start();
